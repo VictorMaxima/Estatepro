4e967c0bad1cb4488aade396fedef05aee24d6b2
VictorMaxima <victormaxchidera@gmail.com>
2026-09-05 18:07:26 +0200

    change

--- estatepro_backend/estatepro/settings.py
+++ estatepro_backend/estatepro/settings.py
@@ -226,6 +226,3 @@ MEDIA_ROOT = BASE_DIR / "media"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,  "static", "assets"),
-]
